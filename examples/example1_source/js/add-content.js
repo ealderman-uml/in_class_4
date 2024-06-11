5184f6e0bed66df7a3a8b679c0618293263ee481
Null <edward_alderman@student.uml.edu>
2024-06-11 19:17:25 -0400

    init
    Initial commit with provided example files

---
+++ examples/example1_source/js/add-content.js
@@ -0,0 +1,8 @@
+//var today = new Date();
+//var hourNow = today.getHours();
+var greeting;
+
+
+greeting = 'Welcome!';
+
+document.write('<h3>' + greeting + '</h3>');
---
+++ examples/example2_source/js/example.js
@@ -0,0 +1,35 @@
+// Create variables for the welcome message
+var greet = ;
+var name = ;
+var message = ;
+// Concatenate the three variables above to create the welcome message
+var welcome =  greet + name + message;
+
+// Create variables to hold details about the sign
+var sign = ;
+var tiles = ;
+var subTotal = ; //$5 for each tile
+var shipping = ;
+var grandTotal = ;
+
+// Get the element that has an id of greeting
+// Replace the content of that element with the personalized welcome message
+
+
+// Get the element that has an id of userSign then update its contents
+
+
+
+// Get the element that has an id of tiles then update its contents
+
+
+
+// Get the element that has an id of subTotal then update its contents
+
+
+
+// Get the element that has an id of shipping then update its contents
+
+
+
+// Get the element that has an id of grandTotal then update its contents
