f3a6fe3cf2f619068609f6e56c9c3730f73d00d6
Null <edward_alderman@student.uml.edu>
2024-06-11 20:02:42 -0400

    completed

--- examples/example1_source/js/add-content.js
+++ examples/example1_source/js/add-content.js
@@ -1,8 +1,19 @@
-//var today = new Date();
-//var hourNow = today.getHours();
+var today = new Date();
+var hourNow = today.getHours(); // returns and integer in range 0 - 23
 var greeting;
 
 
-greeting = 'Welcome!';
+if (hourNow >= 0 && hourNow <= 12) {        // (0 <= hourNow <= 12)
+    greeting = 'Good morning!';
+} else if (hourNow > 12 && hourNow <= 18) { // (12 < hourNow <= 18)
+    /* note: the assignment is lacking an exclamation point for this greeting, 
+     * so I have subsequently omited it here as well. */
+    greeting = 'Good afternoon';
+} else if (hourNow > 18 && hourNow <= 23) { // (18 < hourNow <= 23)
+    greeting = 'Good evening!';
+} else {    // default case
+    greeting = 'Welcome,';
+}
+
 
 document.write('<h3>' + greeting + '</h3>');
--- examples/example2_source/js/example.js
+++ examples/example2_source/js/example.js
@@ -1,35 +1,39 @@
 // Create variables for the welcome message
-var greet = ;
-var name = ;
-var message = ;
+var greet = 'Hello ';
+var name = 'Molly';
+var message = ', please check your order:';
 // Concatenate the three variables above to create the welcome message
 var welcome =  greet + name + message;
 
 // Create variables to hold details about the sign
-var sign = ;
-var tiles = ;
-var subTotal = ; //$5 for each tile
-var shipping = ;
-var grandTotal = ;
+var sign = 'Welcome Home';
+var tiles = sign.length;    // includes whitespace
+var subTotal = tiles * 5;   // $5 for each tile
+var shipping = 7;
+var grandTotal = subTotal + shipping;
 
 // Get the element that has an id of greeting
-// Replace the content of that element with the personalized welcome message
+var greetingDiv = document.getElementById('greeting');
 
+// Replace the content of that element with the personalized welcome message
+greetingDiv.textContent = welcome;
 
 // Get the element that has an id of userSign then update its contents
-
-
+var userSignTd = document.getElementById('userSign');
+userSignTd.textContent = sign;
 
 // Get the element that has an id of tiles then update its contents
-
-
+var tilesTd = document.getElementById('tiles');
+tilesTd.textContent = tiles;
 
 // Get the element that has an id of subTotal then update its contents
-
-
+var subtotalTd = document.getElementById('subTotal');
+subtotalTd.textContent = '$' + subTotal;
 
 // Get the element that has an id of shipping then update its contents
-
-
+var shippingTd = document.getElementById('shipping');
+shippingTd.textContent = '$' + shipping;
 
 // Get the element that has an id of grandTotal then update its contents
+var grandTotalTd = document.getElementById('grandTotal');
+grandTotalTd.textContent = '$' + grandTotal;
